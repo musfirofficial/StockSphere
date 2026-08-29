8fe94ea9c5a9c7cf980ca9ab495d0b1076f7e3e0
musfirofficial <mtamusfirofficial@gmail.com>
2026-08-29 23:44:42 +0530

    Initate a rework

--- backend/app/app.py
+++ backend/app/app.py
@@ -3,8 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.docs import get_swagger_ui_html
 from contextlib import asynccontextmanager
 from app.database import create_db_and_tables, engine, async_session_maker
-from app.init_admin import AdminService
-from app.seed import seed_database
+
+
 
 # Route imports
 from app.routes.v1.user import router as user_router
@@ -25,19 +25,6 @@ from app.routes.v1.stockalert import router as stockalert_router
 async def lifespan(_app: FastAPI):
     # Startup
     await create_db_and_tables()
-
-    # Create default admin if not exists
-    async with async_session_maker() as session:
-        created = await AdminService.create_admin(session)
-        if created == "created":
-            print("[OK] Default admin created")
-        elif created == "updated":
-            print("[OK] Admin Reset Password")
-        else:
-            print("[OK] Admin already exists")
-
-    await seed_database()
-    yield
     # Shutdown
     await engine.dispose()
     # await Post.metadata.drop_all(bind=engine) #!DANGER! This will delete the entire database on shutdown, use with caution!
@@ -54,20 +41,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-# 2. Create a custom offline route for your docs
-@app.get("/docs", include_in_schema=False)
-async def custom_swagger_ui_html():
-    return get_swagger_ui_html(
-        # Adding 'or ""' ensures Pylance always sees a string, fixing the error
-        openapi_url=app.openapi_url or "",
-        title=app.title + " - Swagger UI",
-        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-        swagger_js_url="/static/swagger-ui-bundle.js",
-        swagger_css_url="/static/swagger-ui.css",
-    )
-
 
 app.include_router(user_router)
 app.include_router(category_router)
